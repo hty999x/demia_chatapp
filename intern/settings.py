d3bc95b16d2be17aae4f6e9e3f36c98bbffafcb5
hty999a <hty999a@gmail.com>
2024-04-16 15:13:18 +0900

    202404161513

--- intern/settings.py
+++ intern/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ozyl(r!*=wht$a7^pp+wp=zg5g96yg5wz!7fwe$gq63874z9##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
